f7af5d2f2bd73f57c74bba075d211e2e28a0b8cf
Faiem-qy <f.quanyoung@gmail.com>
2024-06-05 21:00:59 -0400

    make classes through the input.css file

---
+++ Functions-Directives/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ['./index.html', './src/**/*.js'], // Adjust according to your file structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
