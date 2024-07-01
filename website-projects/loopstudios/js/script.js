a13a02992cf3c39cee0ae0fb3c0dfcb6d62b44cc
Faiem-qy <f.quanyoung@gmail.com>
2024-06-30 21:54:06 -0400

    complete function to handle click events for the hamburger menu, fix bugs

--- website-projects/loopstudios/js/script.js
+++ website-projects/loopstudios/js/script.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
-addEventListener('click', navToggle);
+btn.addEventListener('click', navToggle);
 
 function navToggle() {
   btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
 }
