618db57c8815960ce663bd20a826a9b15269c42d
Faiem-qy <f.quanyoung@gmail.com>
2024-07-06 13:11:29 -0400

    link script to index.html& make and test function for event listener

--- website-projects/shortly/js/script.js
+++ website-projects/shortly/js/script.js
@@ -3,3 +3,8 @@ const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
 linkForm.addEventListener('submit', formSubmit);
+
+function formSubmit (e) {
+  e.preventDefault()
+  console.log(123);
+}
