6be4125c260eea78742cdc559105553d54236006
Faiem-qy <f.quanyoung@gmail.com>
2024-07-18 22:26:30 -0400

    add and link file, make variables for toggle

---
+++ website-projects/fylo/js/script.js
@@ -0,0 +1,3 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtn = document.getElementById('theme-toggle-dark-icon');
+const themeToggleBtn = document.getElementById('theme-toggle-light-icon');
