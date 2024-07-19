d2681c78afdc1411186e24ba3ad00aa2723d425f
Faiem-qy <f.quanyoung@gmail.com>
2024-07-18 22:31:47 -0400

    use if statement to check the current state ..dark or light mode

--- website-projects/fylo/js/script.js
+++ website-projects/fylo/js/script.js
@@ -1,3 +1,16 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
-const themeToggleBtn = document.getElementById('theme-toggle-dark-icon');
-const themeToggleBtn = document.getElementById('theme-toggle-light-icon');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  // show light icon
+  themeToggleLightIcon.classList.remove('hidden');
+} else {
+  themeToggleDarkIcon.classList.remove('hidden');
+}
+
